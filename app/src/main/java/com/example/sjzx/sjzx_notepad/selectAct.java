5c34d89f27831e97e2ecd661c6eaacb4f41cee6a
chenchunlin <chenchunlinniu@163.com>
2016-08-06 15:06:55 +0800

    delete and back

---
+++ app/src/main/java/com/example/sjzx/sjzx_notepad/selectAct.java
@@ -0,0 +1,50 @@
+package com.example.sjzx.sjzx_notepad;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class selectAct extends AppCompatActivity implements View.OnClickListener{
+    private Button s_delete,s_back;
+    private TextView s_tv;
+    private NotesDB notesDB;
+    private SQLiteDatabase dbWriter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.select);
+
+        s_delete = (Button)findViewById(R.id.s_delete);
+        s_back = (Button)findViewById(R.id.s_back);
+        s_tv = (TextView)findViewById(R.id.s_tv);
+        notesDB = new NotesDB(this);
+        dbWriter = notesDB.getWritableDatabase();
+        s_back.setOnClickListener(this);
+        s_delete.setOnClickListener(this);
+
+        s_tv.setText(getIntent().getStringExtra(NotesDB.CONTENT));
+
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.s_delete:
+            deleteDate();
+                finish();
+                break;
+            case R.id.s_back:
+                finish();
+                break;
+
+        }
+    }
+    public void deleteDate(){
+
+        dbWriter.delete(NotesDB.TABLE_NAME,"_id=" + getIntent().getIntExtra(NotesDB.ID,0),null);
+
+    }
+}
--- app/src/main/java/com/example/sjzx/sjzx_notepad/text_display.java
+++ app/src/main/java/com/example/sjzx/sjzx_notepad/text_display.java
@@ -1,8 +1,11 @@
 package com.example.sjzx.sjzx_notepad;
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 public class text_display  extends AppCompatActivity{
@@ -10,6 +13,8 @@ public class text_display  extends AppCompatActivity{
     private MyAdapter adapter;
     private NotesDB notesDB;
     private SQLiteDatabase dbReader;
+    private Cursor cursor;
+    private Intent i;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +27,26 @@ public class text_display  extends AppCompatActivity{
         lv =(ListView)findViewById(R.id.list_text);
         notesDB = new NotesDB(this);
         dbReader = notesDB.getReadableDatabase();
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                cursor.moveToPosition(position);
+                Intent i = new Intent(text_display.this, selectAct.class);
+                i.putExtra(NotesDB.ID,
+                        cursor.getInt(cursor.getColumnIndex(NotesDB.ID)));
+                i.putExtra(NotesDB.CONTENT, cursor.getString(cursor
+                        .getColumnIndex(NotesDB.CONTENT)));
+                i.putExtra(NotesDB.TIME,
+                        cursor.getString(cursor.getColumnIndex(NotesDB.TIME)));
+                i.putExtra(NotesDB.IMG,
+                        cursor.getString(cursor.getColumnIndex(NotesDB.IMG)));
+                i.putExtra(NotesDB.VIDEO,
+                        cursor.getString(cursor.getColumnIndex(NotesDB.VIDEO)));
+
+                startActivity(i);
+            }
+        });
     }
 
     public void  selectDB(){
