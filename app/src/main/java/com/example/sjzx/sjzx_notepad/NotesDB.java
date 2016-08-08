1ed8753f50655097cc74925edc77359a60c7d6f6
sjzxlz <2070915749@qq.com>
2016-08-08 16:20:46 +0800

    Add to title

--- app/src/main/java/com/example/sjzx/sjzx_notepad/NotesDB.java
+++ app/src/main/java/com/example/sjzx/sjzx_notepad/NotesDB.java
@@ -9,8 +9,8 @@ public class NotesDB extends SQLiteOpenHelper {
     public static final String TABLE_NAME = "notes";
     public static final String CONTENT = "content";
     public static final String TITLE = "title";
-    public static final String IMG ="img";
-    public static final String VIDEO = "video";
+//    public static final String IMG ="img";
+//    public static final String VIDEO = "video";
     public static final String ID = "_id";
     public static final String TIME = "time";
 
@@ -25,8 +25,8 @@ public class NotesDB extends SQLiteOpenHelper {
         db.execSQL("CREATE TABLE " + TABLE_NAME + " (" + ID
                 + " INTEGER PRIMARY KEY AUTOINCREMENT," + CONTENT
                 + " TEXT NOT NULL,"  + TITLE + " TEXT NOT NULL,"
-                + IMG + " TEXT NOT NULL，"
-                + VIDEO + " TEXT NOT NULL,"
+//                + IMG + " TEXT NOT NULL，"
+//                + VIDEO + " TEXT NOT NULL,"
                 + TIME + " TEXT NOT NULL)");
     }
 
