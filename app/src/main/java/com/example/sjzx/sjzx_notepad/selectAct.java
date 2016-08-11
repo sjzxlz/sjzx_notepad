cd400c9310042763cfc66a093e90112d25d2e590
sjzxlz <2070915749@qq.com>
2016-08-11 17:50:50 +0800

    actionbar添加

--- app/src/main/java/com/example/sjzx/sjzx_notepad/selectAct.java
+++ app/src/main/java/com/example/sjzx/sjzx_notepad/selectAct.java
@@ -3,14 +3,17 @@ import android.app.Dialog;
 import android.content.DialogInterface;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class selectAct extends AppCompatActivity implements View.OnClickListener {
-    private Button s_delete, s_back;
+public class selectAct extends AppCompatActivity  {
+
     private TextView s_tv;
     private TextView s_title_tv;
     private TextView s_time_tv;
@@ -21,45 +24,64 @@ public class selectAct extends AppCompatActivity implements View.OnClickListener
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.select);
-        s_delete = (Button) findViewById(R.id.s_delete);
-        s_back = (Button) findViewById(R.id.s_back);
+
+        //actionbar
+        ActionBar actionBar = this.getSupportActionBar();
+        actionBar.show();
+        //over
+
         s_tv = (TextView) findViewById(R.id.s_tv);
         s_title_tv = (TextView) findViewById(R.id.s_title_tv);
         s_time_tv = (TextView)findViewById(R.id.s_time_tv);
         notesDB = new NotesDB(this);
         dbWriter = notesDB.getWritableDatabase();
 
-        s_back.setOnClickListener(this);
-        s_delete.setOnClickListener(this);
 
         s_title_tv.setText(getIntent().getStringExtra(NotesDB.TITLE));
         s_tv.setText(getIntent().getStringExtra(NotesDB.CONTENT));
         s_time_tv.setText(getIntent().getStringExtra(NotesDB.TIME));
     }
 
+    //Actionbar
     @Override
-    public void onClick(View v) {
-        if (v.getId() == R.id.s_delete) {
-            Dialog dialog = new AlertDialog.Builder(selectAct.this)
-                    .setTitle("删除提示框")
-                    .setIcon(R.mipmap.ic_launcher1)
-                    .setMessage("确认删除吗？")
-                    .setPositiveButton("确认", new DialogInterface.OnClickListener() {
-                        public void onClick(DialogInterface dialog, int which) {
-                            deleteDate();
-                            finish();
-                        }
-                    }).setNegativeButton("取消", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            finish();
-                        }
-                    }).show();
-        }
-        else if (v.getId() == R.id.s_back) {
-                finish();
-        }
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.note_delete, menu);
+        return super.onCreateOptionsMenu(menu);
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_delete:
+                Dialog dialog = new AlertDialog.Builder(selectAct.this)
+                        .setTitle("删除提示框")
+                        .setIcon(R.mipmap.ic_launcher1)
+                        .setMessage("确认删除吗？")
+                        .setPositiveButton("确认", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                deleteDate();
+                                finish();
+                            }
+                        }).setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                finish();
+                            }
+                        }).show();
+                        break;
+            case R.id.action_edit:
+                Toast.makeText(this, "编辑", Toast.LENGTH_SHORT)
+                        .show();
+                finish();
+                break;
+            default:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    //over
+
 
     public void deleteDate(){
         dbWriter.delete(NotesDB.TABLE_NAME,"_id=" + getIntent().getIntExtra(NotesDB.ID,0),null);
